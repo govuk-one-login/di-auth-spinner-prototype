abb815241c09bb6ef8ba41298c82a2445b0db28c
GTVJ <gwyn.jones@digital.cabinet-office.gov.uk>
2024-06-12 10:06:23 +0100

    Add diffing and new content object

--- app/assets/javascripts/spinner.js
+++ app/assets/javascripts/spinner.js
@@ -1,9 +1,38 @@
 const WaitInteractions = (() => {
 
+    const content = {
+        initial: {
+            heading: "Returning you to [service]",
+            spinnerStateText: "Wait to be returned to the service",
+            spinnerState: "pending"
+        },
+        error: {
+            heading: "Sorry, there is a problem",
+            messageText: "We cannot return you to the service at the moment.",
+            whatYouCanDo: {
+                heading: "What you can do",
+                message: {
+                    text1: "Go back to the service you were trying to use. You can look for the service using your search engine or find it from the ",
+                    link: {
+                        href: "https://www.gov.uk",
+                        text: "GOV.UK homepage"
+                    },
+                    text2: "."
+                }
+            }
+        },
+        complete: {
+            spinnerState: "You can now continue to the service"
+        },
+        longWait: {
+            spinnerStateText: "We're still trying to return you to the service"
+        }
+    }
+
     const state = {
-        heading: "Returning you to [service]",
-        spinnerStateText: "Wait to be returned to the service",
-        spinnerState: "pending",
+        heading: content.initial.heading,
+        spinnerStateText: content.initial.spinnerStateText,
+        spinnerState: content.initial.spinnerState,
         buttonDisabled: true,
         done: false,
         virtualDom: [],
@@ -50,12 +79,12 @@ const WaitInteractions = (() => {
             },
             {
                 nodeName: "h2",
-                text: "What you can do",
+                text: content.error.whatYouCanDo.heading,
                 classes: ["govuk-heading-m"]
             },
             {
                 nodeName: "p",
-                innerHTML: `Go back to the service you were trying to use. You can look for the service using your search engine or find it from the <a href="https://www.gov.uk">GOV.UK homepage</a>.`,
+                innerHTML: `${content.error.whatYouCanDo.message.text1}<a href="${content.error.whatYouCanDo.message.link.href}">${content.error.whatYouCanDo.message.link.text}</a>${content.error.whatYouCanDo.message.text2}`,
                 classes: ["govuk-body"]
             },
         ]
@@ -63,12 +92,21 @@ const WaitInteractions = (() => {
         return state.error ? errorState : initialState;
     }
 
+    const vDomHasChanged = (currentVDom, nextVDom) => {
+        return JSON.stringify(currentVDom) !== JSON.stringify(nextVDom);
+    }
+
     const updateDom = () => {
-        document.title = state.heading;
-        state.virtualDom = createVirtualDom();
-        const elements = state.virtualDom.map(convert);
-        const container = document.getElementById("spinner-container")
-        container.replaceChildren(...elements);
+
+        const vDomChanged = vDomHasChanged(state.virtualDom, createVirtualDom());
+        const container = document.getElementById("spinner-container");
+
+        if (vDomChanged) {
+            document.title = state.heading;
+            state.virtualDom = createVirtualDom();
+            const elements = state.virtualDom.map(convert);
+            container.replaceChildren(...elements);
+        }
 
         if (state.error) {
             container.classList.add('spinner-container__error');
@@ -81,14 +119,14 @@ const WaitInteractions = (() => {
 
     const reflectCompletion = () => {
         state.spinnerState = "spinner__ready";
-        state.spinnerStateText = "You can now continue to the service";
+        state.spinnerStateText = content.complete.spinnerState;
         state.buttonDisabled = false;
         state.done = true;
     }
 
     const reflectError = () => {
-        state.heading = "Sorry, there is a problem"
-        state.messageText = "We cannot return you to the service at the moment."
+        state.heading = content.error.heading
+        state.messageText = content.error.messageText
         state.spinnerState = "spinner__failed"
         state.done = true;
         state.error = true;
@@ -123,12 +161,10 @@ const WaitInteractions = (() => {
 
             if (data.status === "Clear to proceed") {
                 reflectCompletion();
-            } else {
-                if (notInErrorOrDoneState()) {
-                    setTimeout(async () => {
-                        await requestIDProcessingStatus();
-                    }, 1000)
-                }
+            } else if (notInErrorOrDoneState()) {
+                setTimeout(async () => {
+                    await requestIDProcessingStatus();
+                }, 1000)
             }
         } catch (e) {
             console.log(e);
@@ -137,11 +173,12 @@ const WaitInteractions = (() => {
     };
 
     return {
+        state: state,
         init: () => {
 
             timers.informUserWhereWaitIsLong = setTimeout(() => {
                 if (state.spinnerState !== "ready") {
-                    state.spinnerStateText = "We're still trying to return you to the service";
+                    state.spinnerStateText = content.longWait.spinnerStateText;
                 }
             }, 5000);
 
@@ -150,7 +187,6 @@ const WaitInteractions = (() => {
             timers.abortUnresponsiveRequest = setTimeout(() => {
                 reflectError();
             }, 15000)
-
 
             updateDom();
 
